04aefa16a7c2474b406fe58b835f4b30f0e25274
Andy Chung <animalyouth@gmail.com>
2010-11-10 02:44:21 -0800

    started adding styles for comments

--- functions.php
+++ functions.php
@@ -304,17 +304,17 @@ function catalog_comment( $comment, $args, $depth ) {
         <div id="comment-<?php comment_ID(); ?>">
         <div class="comment-author vcard">
             <?php echo get_avatar( $comment, 40 ); ?>
-            <?php printf( __( '%s <span class="says">says:</span>', 'twentyten' ), sprintf( '<cite class="fn">%s</cite>', get_comment_author_link() ) ); ?>
+            <?php printf( __( '%s <span class="says">says:</span>', 'catalog' ), sprintf( '<cite class="fn">%s</cite>', get_comment_author_link() ) ); ?>
         </div><!-- .comment-author .vcard -->
         <?php if ( $comment->comment_approved == '0' ) : ?>
-            <em><?php _e( 'Your comment is awaiting moderation.', 'twentyten' ); ?></em>
+            <em><?php _e( 'Your comment is awaiting moderation.', 'catalog' ); ?></em>
             <br />
         <?php endif; ?>
 
         <div class="comment-meta commentmetadata"><a href="<?php echo esc_url( get_comment_link( $comment->comment_ID ) ); ?>">
             <?php
                 /* translators: 1: date, 2: time */
-                printf( __( '%1$s at %2$s', 'twentyten' ), get_comment_date(),  get_comment_time() ); ?></a><?php edit_comment_link( __( '(Edit)', 'twentyten' ), ' ' );
+                printf( __( '%1$s at %2$s', 'catalog' ), get_comment_date(),  get_comment_time() ); ?></a><?php edit_comment_link( __( '(Edit)', 'catalog' ), ' ' );
             ?>
         </div><!-- .comment-meta .commentmetadata -->
 
@@ -331,7 +331,7 @@ function catalog_comment( $comment, $args, $depth ) {
         case 'trackback' :
     ?>
     <li class="post pingback">
-        <p><?php _e( 'Pingback:', 'twentyten' ); ?> <?php comment_author_link(); ?><?php edit_comment_link( __('(Edit)', 'twentyten'), ' ' ); ?></p>
+        <p><?php _e( 'Pingback:', 'catalog' ); ?> <?php comment_author_link(); ?><?php edit_comment_link( __('(Edit)', 'catalog'), ' ' ); ?></p>
     <?php
             break;
     endswitch;
